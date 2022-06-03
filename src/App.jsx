3c0c3295bd0dd1060af99bf676fc094a90ac175b
Rey Gienaldo Jonathan <rey.jonathan@kiranatama.com>
2022-06-03 16:37:19 +0700

    Init project with vite

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className='App'>
+      <header className='header'>
+        <h3>Dragatron.</h3>
+      </header>
+    </div>
+  );
+}
+
+export default App;
