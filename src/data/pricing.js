2b651dc3802cfd468ce3b7f39781fab65ca6e0f5
Rey Gienaldo Jonathan <rey.jonathan@kiranatama.com>
2022-06-04 23:37:41 +0700

    Add pricing plan data

---
+++ src/data/pricing.js
@@ -0,0 +1,20 @@
+export const pricing = [
+  {
+    id: "1",
+    title: "all access membership",
+    price: "299",
+    popular: false,
+  },
+  {
+    id: "2",
+    title: "in person training membership",
+    price: "199",
+    popular: true,
+  },
+  {
+    id: "3",
+    title: "virtual training",
+    price: "99",
+    popular: false,
+  },
+];
