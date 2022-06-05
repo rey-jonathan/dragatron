b88e9abdc76da2df0b404c6d4e797e7671b896e2
Rey Gienaldo Jonathan <rey.jonathan@kiranatama.com>
2022-06-05 13:40:23 +0700

    Fix some styling and add font

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,11 +5,7 @@ import "./header.css";
 const Header = () => {
   return (
     <header className='header'>
-      <FaArrowLeft
-        style={{ fontSize: "30px" }}
-        className='header__arrow'
-        color='#fec465'
-      />
+      <FaArrowLeft className='header__arrow' color='#fec465' />
       <h3 className='header__text'>Dragatron.</h3>
     </header>
   );
--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -1,23 +1,25 @@
 import MembershipBox from "./MembershipBox";
 import { pricing } from "../data/pricing";
 
+import "./MainContent.css";
+
 const MainContent = () => {
   return (
-    <div style={{ width: "75%", margin: "auto" }}>
-      <h3>
+    <div className='main'>
+      <h1>
         SIGN UP AS A <span style={{ color: "#fec465" }}>PROFESSIONAL</span>
-      </h3>
-      <p>
+      </h1>
+      <p className='main__text'>
         <span style={{ color: "#fec465" }}>STEP 3/4</span> SELECT TRAINER
         SUBSCRIPTION
       </p>
       <p>
-        With 3 types of membership,{" "}
+        With 3 Rypes of membership,{" "}
         <span style={{ color: "#fec465" }}>
           Discover the membership which fits your personal needs.
         </span>
       </p>
-      <div style={{ display: "flex" }}>
+      <div className='pricing__box'>
         <MembershipBox data={pricing} />
       </div>
     </div>
